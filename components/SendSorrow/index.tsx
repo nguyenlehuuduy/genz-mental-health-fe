b255babed51c943fab59380efcc202c83c4c000c
nguyenlehuuduy <nguyenlehuuduy.tienphuoc@gmail.com>
2024-11-08 14:28:08 +0700

    eploy source to vercel

--- components/SendSorrow/index.tsx
+++ components/SendSorrow/index.tsx
@@ -39,7 +39,7 @@ export default function SendSorrow({ listSound }: { listSound: any }) {
 
   const handleSubmit = async () => {
     if (message) {
-      setMsgMentalHeath({ mentalHeathQuote: '', arrayQuote: ('Thả lỏng và thư giãn một tí cậu nhé !') })
+      setMsgMentalHeath({ mentalHeathQuote: '', arrayQuote: ['Thả lỏng và thư giãn một tí cậu nhé !','Tớ ở đây cùng cậu'] })
       playAudio();
       await aiMentalHeathQuote(message).then(res => {
         if (res) {
